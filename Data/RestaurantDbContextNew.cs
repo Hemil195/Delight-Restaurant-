5ba7e56184b4d1c9bd84410f8fa10b19b86292eb
hemil195 <hemilpatel0195@gmail.com>
2025-09-21 14:44:37 +0530

    Fix error

---
+++ Data/RestaurantDbContextNew.cs
@@ -0,0 +1,5 @@
+// This file should be removed from the project - placeholder to fix compilation
+namespace RestaurantOrderSystem.Data
+{
+    // This class is not used - implementation is in RestaurantDbContext.cs
+}
